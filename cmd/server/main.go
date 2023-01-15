8e5207fc5cc71b0d0c94cf1d97093ecce2877dbc
Denis Davydov <denis.davydov@kaspersky.com>
2023-01-16 02:10:35 +0300

    Server stub

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,23 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func main() {
+	metricsHandler := func(w http.ResponseWriter, req *http.Request) {
+		if req.Method != http.MethodPost {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			io.WriteString(w, "Method Not Allowed\n")
+			return
+		}
+
+		fmt.Println(req.URL)
+	}
+
+	http.HandleFunc("/", metricsHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
