bdad5eabe9fec73655a3a9a7bb93a8979ec21024
Denis Davydov <denis.davydov@kaspersky.com>
2023-01-17 21:49:39 +0300

    Refactor types

---
+++ internal/common/types/counter.go
@@ -0,0 +1,33 @@
+package types
+
+import (
+	"errors"
+	"strconv"
+)
+
+const CounterTypeName string = "counter"
+
+type Counter int64
+
+func NewCounterFromString(val string) (Counter, error) {
+	intVal, err := strconv.ParseInt(val, 10, 64)
+	if err != nil {
+		return 0, err
+	}
+	if intVal < 0 {
+		return 0, errors.New("value below zero")
+	}
+	return Counter(intVal), nil
+}
+
+func (c Counter) String() string {
+	return strconv.FormatInt(int64(c), 10)
+}
+
+func (c Counter) TypeName() string {
+	return CounterTypeName
+}
+
+func (c Counter) StringValue() string {
+	return c.String()
+}
