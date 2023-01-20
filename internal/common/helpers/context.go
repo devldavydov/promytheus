7752b3e9c2c753d35232de7e3540b28e064217c1
Denis Davydov <denis.davydov@kaspersky.com>
2023-01-20 20:24:12 +0300

    Refactor common context

--- internal/common/helpers/context.go
+++ internal/common/helpers/context.go
@@ -2,27 +2,16 @@ package helpers
 
 import (
 	"context"
-	"os"
 	"os/signal"
 	"syscall"
 )
 
-func CreateContextWithSignalHadler(ctxParent context.Context) context.Context {
-	ctx, cancel := context.WithCancel(ctxParent)
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+func CreateContextWithSignalHadler(parent context.Context) context.Context {
+	ctx, stop := signal.NotifyContext(parent, syscall.SIGINT, syscall.SIGTERM)
 
 	go func() {
-		defer func() {
-			signal.Stop(c)
-			cancel()
-		}()
-
-		select {
-		case <-c:
-			cancel()
-		case <-ctx.Done():
-		}
+		defer stop()
+		<-ctx.Done()
 	}()
 
 	return ctx
