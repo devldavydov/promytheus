ea827cb9dc3ba352b422e19544c1ff2a4768198f
Denis Davydov <denis.davydov@kaspersky.com>
2023-01-23 23:33:58 +0300

    Refactor send metrics

--- internal/agent/metrics/publisher.go
+++ internal/agent/metrics/publisher.go
@@ -2,12 +2,16 @@ package metrics
 
 import (
 	"errors"
+	"fmt"
 	"net/http"
 	"net/url"
 	"time"
 
+	"github.com/devldavydov/promytheus/internal/common/types"
 	"github.com/sirupsen/logrus"
 )
+
+const httpClientTimeout = 1 * time.Second
 
 type Publisher interface {
 	Publish(metrics Metrics) error
@@ -21,7 +25,7 @@ type HTTPPublisher struct {
 
 func NewHTTPPublisher(serverAddress *url.URL, logger *logrus.Logger) *HTTPPublisher {
 	client := &http.Client{
-		Timeout: 1 * time.Second,
+		Timeout: httpClientTimeout,
 	}
 
 	return &HTTPPublisher{serverAddress: serverAddress, httpClient: client, logger: logger}
@@ -30,23 +34,13 @@ func NewHTTPPublisher(serverAddress *url.URL, logger *logrus.Logger) *HTTPPublis
 func (httpPublisher *HTTPPublisher) Publish(metrics Metrics) error {
 	metricsSentCnt := 0
 
+	var err error
 	for name, value := range metrics {
-		request, err := http.NewRequest(
-			http.MethodPost,
-			httpPublisher.serverAddress.JoinPath("update", value.TypeName(), name, value.String()).String(),
-			nil)
+		err = httpPublisher.publishMetric(name, value)
 		if err != nil {
-			httpPublisher.logger.Errorf("Failed to create publish metrics request: %v", err)
+			httpPublisher.logger.Errorf("Failed to publish metric: %v", err)
 			continue
 		}
-		request.Header.Set("Content-Type", "text/plain")
-
-		response, err := httpPublisher.httpClient.Do(request)
-		if err != nil {
-			httpPublisher.logger.Errorf("Failed to publish metrics: %v", err)
-			continue
-		}
-		response.Body.Close()
 
 		metricsSentCnt += 1
 	}
@@ -54,5 +48,25 @@ func (httpPublisher *HTTPPublisher) Publish(metrics Metrics) error {
 	if metricsSentCnt == 0 {
 		return errors.New("failed to publish all metrics")
 	}
+	return nil
+}
+
+func (httpPublisher *HTTPPublisher) publishMetric(metricName string, metricValue types.MetricValue) error {
+	request, err := http.NewRequest(
+		http.MethodPost,
+		httpPublisher.serverAddress.JoinPath("update", metricValue.TypeName(), metricName, metricValue.String()).String(),
+		nil)
+	if err != nil {
+		return fmt.Errorf("failed to create request: %w", err)
+	}
+
+	request.Header.Set("Content-Type", "text/plain")
+
+	response, err := httpPublisher.httpClient.Do(request)
+	if err != nil {
+		return fmt.Errorf("failed to send request: %w", err)
+	}
+	defer response.Body.Close()
+
 	return nil
 }
