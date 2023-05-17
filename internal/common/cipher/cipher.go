28376e7a16a8924708fbf7703011a5c6a250bcc8
devldavydov <devldavydov@yandex.ru>
2023-05-18 01:04:37 +0300

    Upd cipher

--- internal/common/cipher/cipher.go
+++ internal/common/cipher/cipher.go
@@ -4,14 +4,90 @@ import (
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/sha512"
+	"crypto/x509"
+	"encoding/pem"
+	"errors"
 )
 
-func LoadPublicKey(filePath string) (*rsa.PublicKey, error) {
+func PublicKeyFromFile(filePath string) (*rsa.PublicKey, error) {
 	return nil, nil
 }
 
-func LoadPrivateKey(filePath string) (*rsa.PrivateKey, error) {
+func PrivateKeyFromFile(filePath string) (*rsa.PrivateKey, error) {
 	return nil, nil
+}
+
+func GenerateKeyPair(bits int) (*rsa.PrivateKey, *rsa.PublicKey, error) {
+	privkey, err := rsa.GenerateKey(rand.Reader, bits)
+	if err != nil {
+		return nil, nil, err
+	}
+	return privkey, &privkey.PublicKey, nil
+}
+
+func PrivateKeyToBytes(priv *rsa.PrivateKey) []byte {
+	privBytes := pem.EncodeToMemory(
+		&pem.Block{
+			Type:  "RSA PRIVATE KEY",
+			Bytes: x509.MarshalPKCS1PrivateKey(priv),
+		},
+	)
+
+	return privBytes
+}
+
+func PublicKeyToBytes(pub *rsa.PublicKey) ([]byte, error) {
+	pubASN1, err := x509.MarshalPKIXPublicKey(pub)
+	if err != nil {
+		return nil, err
+	}
+
+	pubBytes := pem.EncodeToMemory(&pem.Block{
+		Type:  "RSA PUBLIC KEY",
+		Bytes: pubASN1,
+	})
+
+	return pubBytes, nil
+}
+
+func BytesToPrivateKey(priv []byte) (*rsa.PrivateKey, error) {
+	block, _ := pem.Decode(priv)
+	enc := x509.IsEncryptedPEMBlock(block)
+	b := block.Bytes
+	var err error
+	if enc {
+		b, err = x509.DecryptPEMBlock(block, nil)
+		if err != nil {
+			return nil, err
+		}
+	}
+	key, err := x509.ParsePKCS1PrivateKey(b)
+	if err != nil {
+		return nil, err
+	}
+	return key, nil
+}
+
+func BytesToPublicKey(pub []byte) (*rsa.PublicKey, error) {
+	block, _ := pem.Decode(pub)
+	enc := x509.IsEncryptedPEMBlock(block)
+	b := block.Bytes
+	var err error
+	if enc {
+		b, err = x509.DecryptPEMBlock(block, nil)
+		if err != nil {
+			return nil, err
+		}
+	}
+	ifc, err := x509.ParsePKIXPublicKey(b)
+	if err != nil {
+		return nil, err
+	}
+	key, ok := ifc.(*rsa.PublicKey)
+	if !ok {
+		return nil, errors.New("not public key")
+	}
+	return key, nil
 }
 
 func EncryptWithPublicKey(msg []byte, pub *rsa.PublicKey) ([]byte, error) {
--- internal/common/cipher/cipher_test.go
+++ internal/common/cipher/cipher_test.go
@@ -1,7 +1,78 @@
 package cipher
 
-import "testing"
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestEncryptDecrypt(t *testing.T) {
+	privKeyPath, pubKeyPath, err := generateTempKeyPair()
+	assert.NoError(t, err)
+	defer func() {
+		os.Remove(privKeyPath)
+		os.Remove(pubKeyPath)
+	}()
 
+	pubKey, err := PublicKeyFromFile(pubKeyPath)
+	assert.NoError(t, err)
+
+	privKey, err := PrivateKeyFromFile(privKeyPath)
+	assert.NoError(t, err)
+
+	testMsg := []byte("Hello world!")
+
+	encrMsg, err := EncryptWithPublicKey(testMsg, pubKey)
+	assert.NoError(t, err)
+
+	decrMsg, err := DecryptWithPrivateKey(encrMsg, privKey)
+	assert.NoError(t, err)
+
+	assert.Equal(t, testMsg, decrMsg)
+}
+
+func generateTempKeyPair() (string, string, error) {
+	privkey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return "", "", err
+	}
+
+	//
+	privBytes := pem.EncodeToMemory(
+		&pem.Block{
+			Type:  "RSA PRIVATE KEY",
+			Bytes: x509.MarshalPKCS1PrivateKey(privkey),
+		},
+	)
+
+	pubASN1, err := x509.MarshalPKIXPublicKey(&privkey.PublicKey)
+	if err != nil {
+		return "", "", err
+	}
+	pubBytes := pem.EncodeToMemory(&pem.Block{
+		Type:  "RSA PUBLIC KEY",
+		Bytes: pubASN1,
+	})
+
+	//
+	privFile, err := os.CreateTemp("", "key")
+	if err != nil {
+		return "", "", err
+	}
+	privFile.Write(privBytes)
+	privFile.Close()
+
+	pubFile, err := os.CreateTemp("", "key")
+	if err != nil {
+		return "", "", err
+	}
+	pubFile.Write(pubBytes)
+	pubFile.Close()
+
+	return privFile.Name(), pubFile.Name(), nil
 }
